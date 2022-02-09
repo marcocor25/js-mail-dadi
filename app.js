e46b414d8b74d1bbd3e3921aeac8d442c83c444f
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-02-09 18:38:16 +0100

    Prima parte email completata

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
 // COLLEGO IL BOTTONE PER PRENDERE L'EMAIL DELL'UTENTE
-
 const getEmail = document.getElementById('email-button');
 console.log(getEmail);
 
 // CREO UNA VARIABILE PER PRENDERE IL VALUE DELL'INPUT EMAIL
+const emailInput = document.getElementById('email-box');
+console.log(emailInput);
 
-const email = document.getElementById('email-box');
-console.log(email);
+// CREO UNA VARIABILE PER SAPERE SE HO TROVATO UNA EMAIL VALIDA
+let notAllowedEmail = true;
+// LA VARIABILE HA VALORE TRUE QUANDO L'EMAIL INSERITA DALL'UTENTE NON È VALIDA
+// ALTRIMENTI HA VALORE FALSO
+
+// CREO ARRAY DI EMAIL AMMESSE
+const allowedEmail = [
+    'marco.corallo.1997@gmail.com',
+    'ciccio@pasticcio.it',
+    'ajeje@brazorf.it',
+    'mario@rossi.it',
+    'homer@simpson.com',
+    'bart@simpson.it'
+];
+console.log(allowedEmail);
 
 getEmail.addEventListener('click', function() {
-    alert(email.value);
+    for (let i = 0; i < allowedEmail.length; i++) {
+        if (emailInput.value == allowedEmail[i]) {
+            notAllowedEmail = false;
+        }
+    }
+    
+    if(notAllowedEmail == true){
+        alert('Email errata!');
+        console.log('Email errata!');
+    }
+    else{
+        alert('Email corretta, puoi entrare!');
+        console.log('Email corretta, puoi entrare!');
+    }
 })
