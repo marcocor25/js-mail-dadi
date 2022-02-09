dfc2823ed40b48b933aaffeb90414ef5ccc259bd
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-02-09 17:03:40 +0100

    .

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-// TEST
+// COLLEGO IL BOTTONE PER PRENDERE L'EMAIL DELL'UTENTE
 
-console.log('Ciao a tutti!')
+const getEmail = document.getElementById('email-button');
+console.log(getEmail);
+
+// CREO UNA VARIABILE PER PRENDERE IL VALUE DELL'INPUT EMAIL
+
+const email = document.getElementById('email-box');
+console.log(email);
+
+getEmail.addEventListener('click', function() {
+    alert(email.value);
+})
